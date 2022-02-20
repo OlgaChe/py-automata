697b731c51375a5eceb4b24b722596032dce204e
Olga <o.cherniak@kasko2go.com>
2022-02-20 17:20:19 +0200

    removed allure fixed assert

--- partner_api/authentication/auth_config.py
+++ partner_api/authentication/auth_config.py
@@ -3,6 +3,8 @@ import uuid
 CONF = {
     "auth": {
         "client_id": "login",
-        "client_secret": "password"
+        "client_secret": "password",
+        "client_id_neg": "login",
+        "client_secret_neg": "password"
     }
 }
