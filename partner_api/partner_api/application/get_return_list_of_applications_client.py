cb14e5decef415c700d173f5124e57307dde83f3
Olga <o.cherniak@kasko2go.com>
2022-06-21 16:45:27 +0300

    added application - get list of applications

---
+++ partner_api/partner_api/application/get_return_list_of_applications_client.py
@@ -0,0 +1,40 @@
+import requests
+
+# import config
+from partner_api.partner_api import partner_api_config
+
+# set up config
+config = partner_api_config.CONF.get("DEV")
+
+
+# Generates an Access Token
+# GET {{baseUrl}}/authenticate
+def get_return_list_of_applications(token, filter_param, limit, page, direction, sort):
+    url = config.get("baseUrl") + "/application"
+
+    headers = {
+        'Token': token
+    }
+
+    params = {"filter[]": filter_param, 'limit': limit, 'page': page, 'direction': direction, 'sort': sort}
+
+    response = requests.get(url, headers=headers, params=params)
+
+    print("\n")
+    print("///REQUEST///\n")
+    print("GET " + response.request.url + "\n")
+    print("///RESPONSE///\n")
+    print(response.json())
+    print("\n")
+
+    return response
+
+
+token = ''
+filter_param = ''
+limit = 20
+page = 2
+direction = 'desc'
+sort = 'requested_funding'
+
+get_return_list_of_applications(token, filter_param, limit, page, direction, sort)
