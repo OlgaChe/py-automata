5e91e7018100ff09ddee153903041bb7378283c8
Olga <o.cherniak@kasko2go.com>
2022-07-01 15:56:29 +0300

    updated put partial application client and test

--- partner_api/partner_api/partial/test_update_partial_application.py
+++ partner_api/partner_api/partial/test_update_partial_application.py
@@ -1,16 +1,17 @@
+#  pytest -c "DEV" -s -v -m <test_name> --disable-pytest-warnings
+
 from datetime import datetime
 import random
 import sys
-
-from . import put_update_partial_application_client
-
 import pytest
 import json
 
+from . import put_update_partial_application_client
+from partner_api.partner_api.auth import test_generate_access_token
+# from partner_api.partner_api.application.uuid import test_return_single_application
+
 # import config
 from partner_api.partner_api import partner_api_config
-
-#  pytest -c "DEV" -s -v -m <test_name> --disable-pytest-warnings
 
 
 # set up config
@@ -24,19 +25,21 @@ def set_env():
 # Update an partial application
 # POST {{baseUrl}}/partial
 # positive case
-@pytest.mark.parametrize('new_application_json', ["partner_api/partial/sanity_test/post_create_new_partial_application.json"])
+@pytest.mark.parametrize('update_application_json', ["partner_api/partial/sanity_test/post_create_new_partial_application.json"])
 @pytest.mark.sanity
 @pytest.mark.positive
-@pytest.mark.test_put_update_partial_application_positive_all_required_data
-def test_put_update_partial_application_positive_all_required_data(new_application_json):
+@pytest.mark.test_put_update_partial_application_positive_etalon_all_required_data
+def test_put_update_partial_application_positive_etalon_all_required_data(update_application_json):
     """
     Positive case: update partial application with valid data
     """
 
-    token = '3bedf587d3b21eaad83d667e1cf05712314f713a82feb492f681c5b6139ce3a8'
-    uuid = 'test'
+    # get access token
+    token = test_generate_access_token.test_generate_access_token_positive()
 
-    with open(new_application_json) as json_file:
+    uuid = put_update_partial_application_client.config.get("uuid").get("etalon_uuid_edit")
+
+    with open(update_application_json) as json_file:
         json_data = json.load(json_file)
         json_data["owners"][0]["first_name"] = "qa_" + datetime.now().strftime('%m%d%y%H%M%S') + "_FN"
         json_data["owners"][0]["last_name"] = "qa_" + datetime.now().strftime('%m%d%y%H%M%S') + "_LN"
@@ -52,6 +55,7 @@ def test_put_update_partial_application_positive_all_required_data(new_applicati
     assert parse_json["uuid"] == uuid
     assert parse_json["offer_link"] != ""
 
+    # return uuid for future use
     print(parse_json["uuid"])
     return parse_json["uuid"]
 
