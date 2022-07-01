f13d05ab668bcff29e98d0f69f5fe7de2d0ab80f
Olga <o.cherniak@kasko2go.com>
2022-07-01 16:49:50 +0300

    added update an application client and test

---
+++ partner_api/partner_api/application/uuid/put_update_application_client.py
@@ -0,0 +1,33 @@
+import json
+from datetime import datetime
+import random
+import requests
+
+# import config
+from partner_api.partner_api import partner_api_config
+
+# set up config
+config = partner_api_config.CONF.get("DEV")
+
+
+# Update an application
+# PUT {{baseUrl}}/application/:uuid
+def put_update_application(token, new_application_json, uuid):
+    url = config.get("baseUrl") + "/application/" + uuid
+
+    headers = {
+        'Token': token,
+    }
+
+    response = requests.put(url, headers=headers, json=new_application_json)
+
+    print("\n")
+    print("///REQUEST///\n")
+    print("PUT " + response.request.url + "\n")
+    print(json.dumps(json.loads(response.request.body), indent=2))
+    print("\n")
+    print("///RESPONSE///\n")
+    print(json.dumps(response.json(), indent=2))
+    print("\n")
+
+    return response
---
+++ partner_api/partner_api/application/uuid/test_update_application.py
@@ -0,0 +1,61 @@
+#  pytest -c "DEV" -s -v -m <test_name> --disable-pytest-warnings
+
+from datetime import datetime
+import random
+import sys
+import pytest
+import json
+
+from . import put_update_application_client
+from partner_api.partner_api.auth import test_generate_access_token
+# from partner_api.partner_api.application.uuid import test_return_single_application
+
+# import config
+from partner_api.partner_api import partner_api_config
+
+
+# set up config
+@pytest.fixture(autouse=True)
+def set_env():
+    env = sys.argv[2]
+    print("\nEnv is " + env)
+    put_update_application_client.config = partner_api_config.CONF.get(env)
+
+
+# Update an application
+# POST {{baseUrl}}/application
+# positive case
+@pytest.mark.parametrize('update_application_json', ["partner_api/application/sanity_test/post_create_new_application.json"])
+@pytest.mark.sanity
+@pytest.mark.positive
+@pytest.mark.test_put_update_application_positive_etalon_all_required_data
+def test_put_update_application_positive_etalon_all_required_data(update_application_json):
+    """
+    Positive case: update application with valid data
+    """
+
+    # get access token
+    token = test_generate_access_token.test_generate_access_token_positive()
+
+    uuid = put_update_application_client.config.get("uuid").get("etalon_uuid_edit")
+
+    with open(update_application_json) as json_file:
+        json_data = json.load(json_file)
+        json_data["owners"][0]["first_name"] = "qa_" + datetime.now().strftime('%m%d%y%H%M%S') + "_FN"
+        json_data["owners"][0]["last_name"] = "qa_" + datetime.now().strftime('%m%d%y%H%M%S') + "_LN"
+        json_data["owners"][0]["phonenumbers"][0]["number"] = "212" + str(random.randint(0, 9999999)).zfill(10)
+        json_data["owners"][0]["emails"][0]["email"] = "email_owner+" + datetime.now().strftime('%m%d%y%H%M%S') + "@example.com"
+
+    test_response = put_update_application_client.put_update_application(token, json_data, uuid)
+    parse_json = json.loads(json.dumps(test_response.json()))
+
+    assert test_response.status_code is 200
+    assert parse_json["kapitus_application_status"] == "Approved"
+    assert parse_json["uuid"] != ""
+    assert parse_json["uuid"] == uuid
+    assert parse_json["offer_link"] != ""
+
+    # return uuid for future use
+    print(parse_json["uuid"])
+    return parse_json["uuid"]
+
